ad98c2b7754df378265b8cee6c52750e1e2e30d5
RyanBarszcz <113656726+RyanBarszcz@users.noreply.github.com>
2026-07-21 02:57:04 -0400

    cors

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,7 +4,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     FRONTEND_URL: str = "https://telemetry.ryanbarszcz.com"
+
     CLERK_SECRET_KEY: str
+    CLERK_JWKS_URL: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
