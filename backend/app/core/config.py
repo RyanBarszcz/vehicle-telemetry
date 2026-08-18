3df3afd0468462e4783b617fdb22955009cb1503
RyanBarszcz <113656726+RyanBarszcz@users.noreply.github.com>
2026-08-18 00:47:55 -0400

    feat: deploy SQS worker to AWS ECS

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -10,8 +10,6 @@ class Settings(BaseSettings):
     CLERK_ISSUER: str
 
     AWS_REGION: str = "us-east-2"
-    AWS_ACCESS_KEY_ID: str = ""
-    AWS_SECRET_ACCESS_KEY: str = ""
     S3_BUCKET_NAME: str = ""
     SQS_TELEMETRY_QUEUE_URL: str = ""
 
