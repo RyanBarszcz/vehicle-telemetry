67356254cf456dbc07377ab008f5176336e4434c
RyanBarszcz <113656726+RyanBarszcz@users.noreply.github.com>
2026-08-19 00:47:50 -0400

    im confused, i thought i committed this

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -87,3 +87,105 @@ resource "aws_iam_role_policy" "execution_secrets" {
     ]
   })
 }
+
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:RyanBarszcz/vehicle-telemetry:ref:refs/heads/main"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "driveiqGitHubActionsRole"
+
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy" {
+  name = "DriveIQGitHubActionsDeploy"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "ECRAuthentication"
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+      {
+        Sid    = "ECRPushImages"
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
+        ]
+
+        Resource = [
+          aws_ecr_repository.backend.arn,
+          aws_ecr_repository.worker.arn
+        ]
+      },
+      {
+        Sid    = "ECSDeployment"
+        Effect = "Allow"
+
+        Action = [
+          "ecs:RegisterTaskDefinition",
+          "ecs:DescribeTaskDefinition",
+          "ecs:UpdateService",
+          "ecs:DescribeServices"
+        ]
+
+        Resource = "*"
+      },
+      {
+        Sid    = "PassECSRoles"
+        Effect = "Allow"
+
+        Action = [
+          "iam:PassRole"
+        ]
+
+        Resource = [
+          aws_iam_role.ecs_execution.arn,
+          aws_iam_role.backend_task.arn,
+          aws_iam_role.worker_task.arn
+        ]
+      }
+    ]
+  })
+}
