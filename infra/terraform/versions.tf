3e1e7efa6ff346a2aa77a6f953e0343a221dc904
RyanBarszcz <113656726+RyanBarszcz@users.noreply.github.com>
2026-08-19 00:21:23 -0400

    chore: configure remote Terraform state

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 6.57"
     }
   }
+
+  backend "s3" {
+    bucket       = "driveiq-terraform-state-795176248415-us-east-2"
+    key          = "driveiq/terraform.tfstate"
+    region       = "us-east-2"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
